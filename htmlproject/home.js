7df4c828ce6f64397fbbec9d3f59692b4b48c46a
Mai Tran <110996565+mntran24@users.noreply.github.com>
2023-02-20 00:40:22 -0500

    Add files via upload

---
+++ htmlproject/home.js
@@ -0,0 +1,7 @@
+
+document.addEventListener("DOMContentLoaded", function(){
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+    var popoverList = popoverTriggerList.map(function(element){
+        return new bootstrap.Popover(element);
+    });
+});
